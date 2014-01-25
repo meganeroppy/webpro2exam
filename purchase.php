09566a7cb8b8981f4df4a3a1cc831b8861071084
m.roppy; <roppy@hotmail.co.jp;>
2014-01-25 14:56:19 +0900

    test

--- purchase.php
+++ purchase.php
@@ -3,12 +3,10 @@
 		session_start();
 	}
 
-	if(isset($_POST["id"]){
+	if(isset($_POST["id"])){
 		$id = $_POST["id"];
 	}
 
-	
-
-	echo "購入手続きが完了しました.（商品ID：{$id}）";
+	echo "購入手続きが完了しました.（商品ID：{$id[0]}）";
 	include "index.php";
 
